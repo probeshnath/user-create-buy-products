f6b0e5a9a18687a0b21756c5c560417138240a77
probeshnath <probeshn@gmail.com>
2024-01-28 01:50:54 +0600

    all product structure added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,45 @@ const App = () => {
     <main className='bg-red-200 w-screen h-screen'>
       <section className='max-w-7xl mx-auto flex flex-col justify-between h-full w-full px-2'>
         <div>
-          <h2>All products</h2>
+          <h2 className='text-center pt-5 lg:pb-10 text-4xl font-semibold text-purple-700'>All products</h2>
+          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 justify-items-center gap-5'>
+            <div>
+              <h2>Product Name</h2>
+              <p> Price: $40</p>
+              <p>Available: 100</p>
+            </div>
+            <div>
+              <h2>Product Name</h2>
+              <p> Price: $40</p>
+              <p>Available: 100</p>
+              <button>Buy</button>
+            </div>
+            <div>
+              <h2>Product Name</h2>
+              <p> Price: $40</p>
+              <p>Available: 100</p>
+            </div>
+            <div>
+              <h2>Product Name</h2>
+              <p> Price: $40</p>
+              <p>Available: 100</p>
+            </div>
+            <div>
+              <h2>Product Name</h2>
+              <p> Price: $40</p>
+              <p>Available: 100</p>
+            </div>
+            <div>
+              <h2>Product Name</h2>
+              <p> Price: $40</p>
+              <p>Available: 100</p>
+            </div>
+            <div>
+              <h2>Product Name</h2>
+              <p> Price: $40</p>
+              <p>Available: 100</p>
+            </div>
+          </div>
         </div>
         {/* product create section */}
         <div className='bg-purple-500 py-4 px-6 rounded-md'>
@@ -16,7 +54,7 @@ const App = () => {
           </div>
           <div className='flex gap-4'>
             <input className='py-2 px-4 mb-2 outline-none w-full rounded-md' type="file" placeholder='Enter Your Product Image' />
-            <button className=' px-6 bg-green-600 text-lg font-semibold text-white rounded-md '>Create</button>
+            <button className='px-6 bg-green-600 text-lg font-semibold text-white rounded-md '>Create</button>
           </div>
         </div>
       </section>

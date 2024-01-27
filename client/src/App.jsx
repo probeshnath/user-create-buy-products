f4d0b7f019744056b1f490d2aa951ad01f0b1fc0
probeshnath <probeshn@gmail.com>
2024-01-28 01:37:16 +0600

    create new product design added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,20 +3,20 @@ import React from 'react'
 const App = () => {
   return (
     <main className='bg-red-200 w-screen h-screen'>
-      <section className='max-w-7xl mx-auto'>
+      <section className='max-w-7xl mx-auto flex flex-col justify-between h-full w-full px-2'>
         <div>
           <h2>All products</h2>
         </div>
         {/* product create section */}
-        <div>
-          <input type="text" placeholder='Enter Product Name' />
-          <div>
-            <input type="number" placeholder='Enter Product Price' />
-            <input type="number" placeholder='Enter Product Quantity' />
+        <div className='bg-purple-500 py-4 px-6 rounded-md'>
+          <input type="text" className='py-2 px-4 mb-2 outline-none w-full rounded-md' placeholder='Enter Product Name' />
+          <div className='flex gap-4'>
+            <input type="number" className='py-2 px-4 mb-2 outline-none w-full rounded-md' placeholder='Enter Product Price' />
+            <input type="number" className='py-2 px-4 mb-2 outline-none w-full rounded-md' placeholder='Enter Product Quantity' />
           </div>
-          <div>
-            <input type="file" placeholder='Enter Your Product Image' />
-            <button>Create</button>
+          <div className='flex gap-4'>
+            <input className='py-2 px-4 mb-2 outline-none w-full rounded-md' type="file" placeholder='Enter Your Product Image' />
+            <button className=' px-6 bg-green-600 text-lg font-semibold text-white rounded-md '>Create</button>
           </div>
         </div>
       </section>

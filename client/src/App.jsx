a45c1eca821bfe4da7c92ab937830ee691f2d770
probeshnath <probeshn@gmail.com>
2024-01-28 01:00:23 +0600

    basic structure added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,9 +2,25 @@ import React from 'react'
 
 const App = () => {
   return (
-    <h1 className="text-3xl text-red-600 font-bold underline">
-    Hello world!
-  </h1>
+    <main className='bg-red-200 w-screen h-screen'>
+      <section className='max-w-7xl mx-auto'>
+        <div>
+          <h2>All products</h2>
+        </div>
+        {/* product create section */}
+        <div>
+          <input type="text" placeholder='Enter Product Name' />
+          <div>
+            <input type="number" placeholder='Enter Product Price' />
+            <input type="number" placeholder='Enter Product Quantity' />
+          </div>
+          <div>
+            <input type="file" placeholder='Enter Your Product Image' />
+            <button>Create</button>
+          </div>
+        </div>
+      </section>
+    </main>
   )
 }
 
